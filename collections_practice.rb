d22b6f002d72a9ca49734e43fd9a3c043874657c
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-20 18:48:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,9 @@
+#sort_array_asc - sorts in ascending order
+def  sort_array_asc(array)
+  return array.sort{ |a, b| a <=> b }
+end
 
+#
+def sort_array_desc(array)
+  return array.sort{ |a, b| b <=> a }
+end
